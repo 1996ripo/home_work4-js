cdf666e227b73eafdb8c5dd698f7f775b4b80f35
Hripsime <1996ripo@gmail.com>
2022-01-20 08:26:50 -0800

    Homework 1-5

---
+++ script1-4.js
@@ -0,0 +1,57 @@
+// 1--------------anagrams-----
+// let array = ['lleho', 'hello', 'asdasd', 'dasds'];
+
+function anagram(text, arr) {
+    arr.forEach(element => {
+        if (element.split('').sort().join('') === text.split('').sort().join('')) {
+            anagramArr.push(element)
+        }
+    });
+
+    return anagramArr;
+};
+
+console.log(anagram('hello', array));
+
+// 2------------strings----
+
+function stringCrop(textMinus, textLine) {
+   return textLine.split(textMinus).join('')
+};
+
+console.log( stringCrop('o', 'I was able to do it'));
+
+// -----------------------3 MISSING WORDS---
+
+function sentenceMissingWords(text, arr) {
+   let arrText = text.split(' ');
+   for (const [key, value] of Object.entries(arrText)) {
+      if (value === '_') {
+         arrText.splice(key, 1, `${arr[0]}`);
+         arr.shift();
+      }
+   }
+   return arrText.join(' ');
+
+}
+
+
+let arr = ['Houston','problem'];
+console.log(sentenceMissingWords('_ , we have a _ .', arr));
+
+// ------------------4 longest word--------------------
+
+function maxLength(text) {
+   let arrText = text.split(' ');
+   let max = 0;
+   let maxValue;
+   arrText.forEach(element => {
+      if (element.length >= max) {
+         max = element.length
+         maxValue = element;
+      }
+   });
+   return maxValue;
+}
+
+console.log(maxLength('hello hi bay asdfa asgdf asdfs 12345 asdasd_asdasd asdasd, sadasd.'));
---
+++ script5.js
@@ -0,0 +1,61 @@
+// -----------------5---------------
+
+let table = document.getElementById('table');
+let vote = document.getElementById('vote');
+let form = document.getElementById('form');
+let candidatName = document.getElementById('candidat-name');
+let inputValue = '';
+let inputValueHist = [];
+let myArray = ['Yerevan', 'London', 'Moscow', 'Paris'];
+
+
+function getRandomInt(min, max) {
+   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomTown() {
+   return myArray[(Math.random() * myArray.length) | 0];
+}
+
+
+function addRow(value) {
+   table.insertAdjacentHTML('beforeend',
+      `<tr class='row'">  
+            <th class='name'>${value}</th>
+            <th>${getRandomTown(20, 75)}</th>
+            <th>${getRandomTown()}</th>
+            <th class='votes'>1</th> 
+         </tr>`);
+};
+
+candidatName.addEventListener('input', e => {
+   inputValue = e.target.value;
+})
+
+
+form.addEventListener('submit', e => {
+   e.preventDefault();
+   let addInfo = 0;
+   let allVote = [];
+   let test = {};
+   if (inputValue != '') {
+      for (let i = 0; i < inputValueHist.length; i++) {
+         if (inputValueHist[i] === inputValue) {
+            document.getElementsByClassName('votes')[i + 1].innerText =
+               +document.getElementsByClassName('votes')[i + 1].textContent + 1;
+            addInfo = 1;
+         }
+      }
+      if (addInfo == 0) {
+         inputValueHist.push(inputValue);
+         addRow(inputValue);
+      }
+   }
+   // Array.prototype.forEach.call(document.getElementsByClassName('votes'), e => {
+   // });
+   // allVote.shift();
+   // console.log(allVote);
+   // for (let i = 0; i < .length)
+
+});
+
