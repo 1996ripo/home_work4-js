2b49327ce2624981122b6433fd8e197d44d08055
Hripsime <1996ripo@gmail.com>
2022-01-21 11:08:11 -0800

    xndir 5

--- script5.js
+++ script5.js
@@ -11,18 +11,17 @@ let myArray = ['Yerevan', 'London', 'Moscow', 'Paris'];
 
 function getRandomInt(min, max) {
    return Math.floor(Math.random() * (max - min) + min);
-}
+};
 
 function getRandomTown() {
    return myArray[(Math.random() * myArray.length) | 0];
-}
-
+};
 
 function addRow(value) {
    table.insertAdjacentHTML('beforeend',
       `<tr class='row'">  
             <th class='name'>${value}</th>
-            <th>${getRandomTown(20, 75)}</th>
+            <th>${getRandomInt(20, 75)}</th>
             <th>${getRandomTown()}</th>
             <th class='votes'>1</th> 
          </tr>`);
@@ -30,9 +29,10 @@ function addRow(value) {
 
 candidatName.addEventListener('input', e => {
    inputValue = e.target.value;
-})
+});
 
-let obj ={};
+
+let obj = {};
 form.addEventListener('submit', e => {
    e.preventDefault();
    let addInfo = 0;
@@ -43,6 +43,9 @@ form.addEventListener('submit', e => {
          if (inputValueHist[i] === inputValue) {
             document.getElementsByClassName('votes')[i + 1].innerText =
                +document.getElementsByClassName('votes')[i + 1].textContent + 1;
+               // table.insertAdjacentHTML('afterbegin',``));
+               document.getElementsByClassName('votes')[i + 1].parentNode.remove();
+               // console.log(typeof inf);
             addInfo = 1;
          }
       }
@@ -51,16 +54,31 @@ form.addEventListener('submit', e => {
          addRow(inputValue);
       }
    }
-
-   Array.prototype.forEach.call(document.getElementsByClassName('votes'), e => {
-      console.log(e.toString);
-      Object.assign(obj, {[e.textContent]: `${e}`});
-      
-   });
-   console.log(obj);
-   // allVote.shift();
-   // console.log(allVote);
-   // for (let i = 0; i < .length)
-
 });
 
+// form.addEventListener('change', e => {
+//    let a = [];
+//    Array.prototype.forEach.call(document.getElementsByClassName('votes'), e => {
+//       for (i = 0; i < document.getElementsByClassName('votes').length; i++) {
+//          a.push(document.getElementsByClassName('votes')[i]);
+//       };
+//    });
+//    console.log(a);
+// // })
+
+// let array = [];
+// form.addEventListener('change', e => {
+//    let rows =document.getElementsByClassName('row');
+//    console.log(rows[0].lastChild);
+//    // let elems = document.getElementsByClassName("votes");
+  
+//    // for (let i = elems.length >>> 0; i--;) {
+//    //    array[i] = elems[i];
+//    // }
+
+//    // array.sort(function (a, b) {
+//    //    return Number(b.innerHTML) - Number(a.innerHTML);
+//    // });
+//    // console.log(array);
+  
+// })
